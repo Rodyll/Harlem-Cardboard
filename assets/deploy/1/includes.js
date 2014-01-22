2e1fdb5567960d97624c776d24f5ab15f10755e7
rodyll <seb.m18@gmail.com>
2014-01-22 15:18:44 +0100

    Add all files

---
+++ assets/deploy/1/includes.js
@@ -0,0 +1,6 @@
+(mighty.Loader.path = (window.gParams.gPath ? window.gParams.gPath : ""));
+(function(){
+var loader = mighty.Loader;
+loader.include("src/plugins/Server/Server.js");
+loader.include("src/addons/scene/Scene.Default.js");
+})();
--- src/addons/scene/Scene.Default.js
+++ src/addons/scene/Scene.Default.js
@@ -1,79 +1,23 @@
-"use strict";
 
 Scene.Default = Scene.Base.extend
 ({
-    setup: function () {
-        this.setupTemplates();
-        this.setupTextures();
-//		this.setupLevel();
-    },
-
-    setupTemplates: function () {
-		mighty.Template.init();
-		mighty.Template.create("Loading");
-		mighty.Template.create("FPS").show();
-	},
-
-    setupTextures: function () {
+    setup: function()
+    {
+        // Setup and load textures we want to use.
         mighty.LoadTexture([
-            {path: "assets/bg"},
-            {path: "assets/snake"},
-            {path: "assets/food"}
+            { path: "assets/bg" },
+            { path: "assets/snake" },
+            { path: "assets/food" }
         ]);
     },
 
-    load: function () {
-        this.gridWidth = 32;
-        this.gridHeight = 32;
-        this.gridSizeX = 20;
-        this.gridSizeY = 30;
-        Terrain.Cfg.grid.width = this.gridWidth;
-        Terrain.cfg.grid.height = this.gridHeight;
-        this.createLevel({ sizeX: 10, sizeY: 10, texture: "bg"});
-    },
 
-    createMan: function () {
-        //Create man
-        var x = Math.floor(this.gridSizeX / 2);
-        var y = Math.floor(this.gridSizeY / 2);
-        x *= this.gridWidth;
-        y *= this.gridHeight;
-
-        this.man = new Entity.Geometry({x: x, y: y, texture: "snake"});
-        Entity.plugin.addEntity(this.man);
-
-    },
-
-    createCardboardBox: function () {
-        this.boxX = mighty.Random.getNumber(0, this.gridSizeX - 1);
-        this.boxY = mighty.Random.getNumber(0, this.gridSizeY - 1);
-        var x = this.boxX * this.gridWidth;
-        var y = this.boxY * this.gridHeight;
-
-        this.box = new Entity.Geometry({ x: x, y: y, texture: "food" });
-        Entity.plugin.addEntity(this.box);
-    },
-
-    ready: function () {
-        this.direction = "none";
-        this.score = 0;
-
-        this.scoreTxt = new Entity.Text({ x: 5, y: 5 });
-        this.scoreTxt.text = "Score: " + this.score;
-        Entity.plugin.addEntity(this.scoreTxt);
-
-        this.createMan();
-        this.createCardboardBox();
+    textures: function()
+    {
+        mighty.LoadTexture(
+            [{
+                path: "assets/basketcourt"}
+            }]);
     }
-//
-//
-//	setupLevel: function()
-//	{
-//		mighty.engine.blockInput = true;
-//
-//		if(!mighty.editor) {
-//			var levelID = Engine.Cfg.defaultLevel;
-//			this.loadLevel(Palettes.Map.getByID(levelID));
-//		}
-//	}
+
 });
